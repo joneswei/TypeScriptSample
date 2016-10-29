5292dd1c37a37a84cbb504a69d64f2316919609c
jones <wei.jones@gmail.com>
2016-10-29 17:59:46 +0800

    1

---
+++ Day1.ts
@@ -0,0 +1,12 @@
+let value: number = 1;
+let count: number = 6;
+let numberPicked:number[];
+let list: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+    31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49];
+
+//let sentence: string = `The Number Picked up  is  ${name}.`;
+
+
